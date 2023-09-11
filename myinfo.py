7c5bf6c90611f5aa438d88249fb3a0697a525936
Sashank224 <53429130+Sashank224@users.noreply.github.com>
2023-09-11 09:45:12 -0400

    my information v1
    my information

---
+++ myinfo.py
@@ -0,0 +1,11 @@
+# CS175-50
+
+#Your first name and Last name
+
+#myInfo
+
+print ('Sashank Vaddiparty')
+
+print('10413 Maple Leaf Dr, Lawerence Township, New Jersey 08648')
+
+print('718-514-4284')
